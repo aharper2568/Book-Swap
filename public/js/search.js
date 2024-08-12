0c725d1a5ea4a07260c2829d732d5ae7f4275178
aharper2568 <anthonyharper568@gmail.com>
2024-08-12 19:10:15 -0400

    added preliminary front-end search script

---
+++ public/js/search.js
@@ -0,0 +1,21 @@
+document.getElementById('search-form').addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const query = document.getElementById('search-query').value.trim();
+  if (query) {
+    try{
+      const response = await fetch('/api/books/search', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ query }),
+      });
+      if (response.ok) {
+        const books = await response.json();
+        console.log('Retrieved books');
+      } else {
+        console.error('couldnt fetch books')
+      }
+    } catch (err){
+      console.error('failed to fetch', err)
+    }
+  }
+})
