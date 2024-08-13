a1c07b6466feee5d06dc97e1db4333faf58f2da6
aharper2568 <anthonyharper568@gmail.com>
2024-08-12 21:11:36 -0400

    added preliminary test to console log searched books

--- utils/googleBooks.js
+++ utils/googleBooks.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 const searchBooks = async (query) => {
-  const apiKey = proncess.env.GOOGLE_BOOKS_API_KEY;
+  const apiKey = process.env.GOOGLE_BOOKS_API_KEY;
   const url = `https://www.googleapis.com/books/v1/volumes?q=${(query)}&key=AIzaSyBdbftoxdcWYWl4ON9k2FvCpKhwnlFNHIA`
 
   try {
