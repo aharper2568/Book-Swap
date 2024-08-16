5bb6314ecf002a0036c6a43823d6e54c7d0cfc52
aharper2568 <anthonyharper568@gmail.com>
2024-08-15 20:17:30 -0400

    added untested exchange script for collections page

---
+++ public/js/exchange.js
@@ -0,0 +1,32 @@
+
+const exchangeButtons = document.querySelectorAll('.exchange-btn');
+
+
+exchangeButtons.forEach(button => {
+  button.addEventListener('click', async (event) => {
+    const bookId = event.target.getAttribute('data-book-id');
+    const ownerId = event.target.getAttribute('data-owner-id');
+    const userId = document.getElementById('userId').value; 
+
+    try {
+      const response = await fetch('/api/books', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          bookId,
+          currentOwnerId: ownerId,
+          newOwnerId: userId
+        }),
+      });
+
+      if (response.ok) {
+        alert('Exchanged :)');
+        location.reload();
+      } else {
+        alert('no exchange :(');
+      }
+    } catch (err) {
+      console.error('Error:', err);
+    }
+  });
+});
