7ee0aeda4f235fbbf78db0c59be5a711f9f6c55d
aharper2568 <anthonyharper568@gmail.com>
2024-08-19 19:22:20 -0400

    added Swap model and updated model associations to SWAP

--- models/index.js
+++ models/index.js
@@ -1,11 +1,39 @@
 const User = require('./User');
 const Book = require('./Book');
+const Swap = require('./Swap');
 
-Book.belongsTo(User,{
-  foreignKey: 'userId'
-})
+Book.belongsTo(User, {
+  foreignKey: 'userId',
+});
 
 User.hasMany(Book, {
-  foreignKey:'userId'
-})
-module.exports = { User, Book };
+  foreignKey: 'userId',
+});
+
+User.hasMany(Swap, {
+  foreignKey: 'lenderId',
+});
+
+User.hasMany(Swap, {
+  foreignKey: 'borrowerId',
+});
+
+Book.hasMany(Swap, {
+  foreignKey: 'bookId',
+});
+
+Swap.belongsTo(User, {
+  foreignKey: 'lenderId',
+  as: 'lender',
+});
+
+Swap.belongsTo(User, {
+  foreignKey: 'borrowerId',
+  as: 'borrower',
+});
+
+Swap.belongsTo(Book, {
+  foreignKey: 'bookId',
+});
+
+module.exports = { User, Book, Swap };
